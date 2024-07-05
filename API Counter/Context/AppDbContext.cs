445b07fd4bae638e38ba404681ef6ea928fa8112
PhariusHS <lucaponteprimo04@gmail.com>
2024-07-05 14:20:54 +0200

    Db string and DataContext

--- API Counter/Context/AppDbContext.cs
+++ API Counter/Context/AppDbContext.cs
@@ -1,8 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API_Counter.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace API_Counter.Context
 {
     public class AppDbContext: DbContext
     {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        
+
+        
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<ManagerTeam> ManagerTeams { get; set; }
+        public DbSet<PlayerPosition> PlayerPositions { get; set; }
+
     }
 }
