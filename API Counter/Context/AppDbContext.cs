c5ba02a30d60ef63a6160a1ce50679f604a4e22a
PhariusHS <lucaponteprimo04@gmail.com>
2024-07-06 02:41:59 +0200

    Many to many start

--- API Counter/Context/AppDbContext.cs
+++ API Counter/Context/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using API_Counter.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace API_Counter.Context
 {
@@ -13,6 +14,7 @@ namespace API_Counter.Context
         
         }
 
+       //Datacontext
         public DbSet<Player> Players { get; set; }
         public DbSet<Team> Teams { get; set; }
         public DbSet<Manager> Managers { get; set; }
@@ -20,5 +22,20 @@ namespace API_Counter.Context
         public DbSet<ManagerTeam> ManagerTeams { get; set; }
         public DbSet<PlayerPosition> PlayerPositions { get; set; }
 
+
+
+        //Many to Many realtionship
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Manager <-> Teams relationship
+            modelBuilder.Entity<ManagerTeam>()
+                .HasKey(mt => new {mt.Manager_Id, mt.Team_Id});
+            modelBuilder.Entity<ManagerTeam>()
+                .HasOne(m => m.Manager)
+                .WithMany(mt => mt.ManagerTeams)
+                .HasForeignKey(t => t.Team_Id);
+
+        }
     }
 }
