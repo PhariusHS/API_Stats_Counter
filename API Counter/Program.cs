cf7dba25a2b232b3026eef5f3206820813a25f84
PhariusHS <lucaponteprimo04@gmail.com>
2024-07-09 22:59:06 +0200

    ProgramCs Setting

--- API Counter/Program.cs
+++ API Counter/Program.cs
@@ -1,3 +1,6 @@
+using API_Counter.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(op =>
+{
+    op.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
